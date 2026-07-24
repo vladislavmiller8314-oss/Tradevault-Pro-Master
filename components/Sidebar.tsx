0c0451356f354db63772d51c1949c59f9459a4d0
vladislavmiller8314-oss <vladislavmiller8314@t-online.de>
2026-07-24 17:01:12 +0200

    help circle

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { HelpCircle } from "lucide-react";
 import { NAV } from "@/lib/nav";
 import { signOut } from "@/app/login/actions";
 
